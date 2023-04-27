10c1cd6445c72560fe213e04c4398f9f7259e023
Mariusz Najwer <najwer23@gmail.com>
2023-04-27 12:06:58 +0200

    rename variable

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
-import fetchSlice from "./features/fetch/fetchSlice";
+import fetchReducer from "./features/fetch/fetchSlice";
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
-		fetch: fetchSlice,
+		fetch: fetchReducer,
 	},
 });
 
